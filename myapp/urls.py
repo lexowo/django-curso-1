5885541265f8f486f7aabf9959221e37def2fdd6
lexowo <lexwolff@protonmail.ch>
2023-08-10 22:52:32 -0600

    Params y Models

--- myapp/urls.py
+++ myapp/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns=[
     path('', views.index), #dominio principal
     path('about/', views.about),
-    path('hello/<str:username>', views.hello) #(params) entre morelessthan es solo una variable no algo especifico
+    path('hello/<str:username>', views.hello), #(params) entre morelessthan es solo una variable no algo especifico
+    path('projects/', views.projects),
+    path('tasks/<str:title>', views.tasks),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,4 +1,6 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+from .models import Project, Task
+from django.shortcuts import get_object_or_404
 
 # Create your views here.
 def index(request):
@@ -9,3 +11,12 @@ def hello(request, username): #Recibe parametro de django (request) /espera para
 
 def about(request):
     return HttpResponse("About")
+
+def projects(request):
+    projects=list(Project.objects.values())
+    return JsonResponse(projects, safe=False)
+
+def tasks(request, title):
+    #task=get_object_or_404(Task, id=id)
+    task=Task.objects.get(title=title)
+    return HttpResponse('task: %s' % task.title)
